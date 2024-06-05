c415dc267bda9b83dafdb8280a89c0b5e8e4131d
AverageGD <edgargames333@gmail.com>
2024-06-05 14:54:33 +0400

    Update

--- Filter.cs
+++ Filter.cs
@@ -2,8 +2,13 @@
 
 namespace WindowsFormsApp1
 {
-    internal abstract class Filter
+    internal abstract class Filter : IFeature
     {
+        public Bitmap ImplementFeature(MyDelegate del, Image img)
+        {
+            return del(img);
+        }
+
         public abstract Bitmap UseFilter(Image defaultImage);
     }
 }
---
+++ IFeature.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    public delegate Bitmap MyDelegate(Image img);
+    internal interface IFeature
+    {
+        Bitmap ImplementFeature(MyDelegate del, Image img);
+    }
+}
